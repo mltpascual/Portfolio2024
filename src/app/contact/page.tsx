f8e31332c894f2eca7f5758999d98c18f9cd444d
mltpascual <miguelpascual.life@gmail.com>
2024-11-08 10:42:12 -0600

    final update na to

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,6 @@
 import ContactForm from "@/components/ContactForm";
-import { Resend } from "resend";
 
 export default function ContactPage() {
-  const resend = new Resend(process.env.RESEND_API_KEY);
-
   return (
     <article className="mt-8 flex flex-col gap-8 pb-16">
       <h1 className="title">Contact me.</h1>
